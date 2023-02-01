66c3aad58da7d83834ad7738e57e5ef52d0400e8
kyoungwan.chae <kyngwan@gmail.com>
2023-02-01 17:42:42 +0900

    Install aptos

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -6,12 +6,20 @@ import "./styles/reset.scss"
 import "./styles/font.scss"
 import "./styles/globals.scss"
 
+declare global {
+  interface Window {
+    aptos: any
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+window.addEventListener("load", () => {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
