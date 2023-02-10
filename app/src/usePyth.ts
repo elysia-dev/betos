4d65b6ab7d75363cbc6e9802b3f3dfbd52a4e211
kyoungwan.chae <kyngwan@gmail.com>
2023-02-10 20:37:22 +0900

    Remove log

--- app/src/usePyth.ts
+++ app/src/usePyth.ts
@@ -9,7 +9,6 @@ import useAptosModule from "./useAptosModule"
 
 const usePyth = () => {
   const { network } = useAptosModule()
-  console.log("network", network)
   const address = priceServiceAddress[network]
   const testnetConnection = new AptosPriceServiceConnection(address)
 
